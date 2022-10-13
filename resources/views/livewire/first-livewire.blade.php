b77051031318befd2c3be06d101f0a387d9f581b
agung <agungarsenius@gmail.com>
2022-10-13 12:18:00 +0700

    first commit

---
+++ resources/views/livewire/first-livewire.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <input type="text" wire:model="name">
+    <h3> Hi, My name is {{$name}} </h3>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+use App\Http\Controllers\ArticleController;
+use App\Http\Livewire\Article;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/tutorial', function () {
+    return view('tutorial');
+});
+
+Route::get('/users', function () {
+    return view('users.index');
+})->name('users.home');
+
+Route::get('/users/{id}', [UserController::class,'show'])->name('users.details'); 
+
+Route::get('/users/{id}/edit', [UserController::class,'edit'])->name('users.edit'); 
+
+Route::get('article/{name}',Article::class)->name('article');
